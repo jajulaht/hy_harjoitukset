eebeda35bcda7f7ac7943b8251cf324e58642e24
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-09-07 21:38:01 +0300

    unicafe step 3 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,15 +9,33 @@ const Button = (props) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad
+  let avg = 0
+  let pos = 0
+  if (all !== 0) {
+    avg = ((good * 1) + (neutral * 0) + (bad * -1)) / all
+    pos = good / all * 100
+  }
+  return (
+    <>
+    <h2>Statistics</h2>
+    <p>Good {good}<br />
+      Neutral {neutral}<br />
+      Bad {bad}<br />
+      All {all}<br />
+      Average {avg}<br />
+      Positive {pos} %
+    </p>
+    </>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let all = good + neutral + bad
-  let avg = ((good * 1) + (neutral * 0) + (bad * -1)) / all
-  let pos = good / all * 100
 
   const setNewGood = (newValue) => {
     setGood(newValue)
@@ -37,14 +55,7 @@ const App = () => {
       <Button handleClick={ () => setNewGood(good + 1) } text="good" />
       <Button handleClick={ () => setNewNeutral(neutral + 1) } text="neutral" />
       <Button handleClick={ () => setNewBad(bad + 1) } text="bad" />
-      <h2>Statistics</h2>
-      <p>Good {good}<br />
-        Neutral {neutral}<br />
-        Bad {bad}<br />
-        All {all}<br />
-        Average {avg}<br />
-        Positive {pos} %
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
