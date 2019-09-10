755943839422111c9ee5c4e667fdc4ef28fbb63b
Janne Lahtinen <janne.lahtinen@grafinote.fi>
2019-09-10 15:02:23 +0300

    course phone book step 1 done

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const Person = ({ name }) => {
+  return (
+    <span>{name}</span>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState(
+    'Add a new name...'
+  )
+
+  // Handle input fields change event and state
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  // Adding a name to state array
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('Add a new name...')
+  }
+
+  // Map persons array data to rows
+  const rows = () => persons.map(person =>
+    <React.Fragment key={person.name}>
+    <Person
+      name={person.name}
+    /><br />
+    </React.Fragment>
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} 
+                       onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default App
